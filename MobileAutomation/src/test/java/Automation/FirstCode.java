669533ba080c8edf044d055339e4db64b96db346
2304119 <2304119@LTIN517422.cts.com>
2024-09-09 16:13:37 +0530

    resources

--- MobileAutomation/src/test/java/Automation/FirstCode.java
+++ MobileAutomation/src/test/java/Automation/FirstCode.java
@@ -1,5 +1,22 @@
 package Automation;
 
-public class FirstCode {
+import java.net.MalformedURLException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.net.URL;
 
+import org.testng.annotations.Test;
+
+import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.android.options.UiAutomator2Options;
+
+public class FirstCode {
+	
+	@Test()
+	public void basicRun() throws Exception {
+		System.out.println("basic run");
+		UiAutomator2Options options= new UiAutomator2Options();
+		options.setDeviceName("ShivaEmulator");
+		AndroidDriver driver= new AndroidDriver(new URI("http://0.0.0.0:4723").toURL(), options);
+	}
 }
